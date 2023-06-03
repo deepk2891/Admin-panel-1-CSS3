e1893bdc1e34409b57b6802ef83449ecb7b102aa
deepk2891 <deepk2891@gmail.com>
2023-06-03 21:51:00 +0530

    completed Aside bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,4 +3,9 @@ $(document).ready(function () {
         $('.profile-setting').toggleClass('visible');
         $('.profile-setting').toggle();
     });
+    $('.accordion').click(function () {
+        $(this).toggleClass('active');
+        $(this).find('i').toggleClass('fa-angle-right fa-angle-down');
+        $(this).find('ul').slideToggle();
+    });
 });
