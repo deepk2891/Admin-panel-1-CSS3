2517fde623a689d5aefc385a77cadf309db0bf80
deepk2891 <deepk2891@gmail.com>
2023-06-14 23:23:50 +0530

    Added Full Screen feature

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,4 +25,46 @@ $(document).ready(function () {
     $('#DarkModeButton').click(function () {
         $('html').toggleClass('dark-mode');
     });
+    function toggleFullscreen (elem) {
+        elem = elem || document.documentElement;
+        if (
+            !document.fullscreenElement &&
+            !document.mozFullScreenElement &&
+            !document.webkitFullscreenElement &&
+            !document.msFullscreenElement
+        )
+        {
+            if (elem.requestFullscreen)
+            {
+                elem.requestFullscreen();
+            } else if (elem.msRequestFullscreen)
+            {
+                elem.msRequestFullscreen();
+            } else if (elem.mozRequestFullScreen)
+            {
+                elem.mozRequestFullScreen();
+            } else if (elem.webkitRequestFullscreen)
+            {
+                elem.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+            }
+        } else
+        {
+            if (document.exitFullscreen)
+            {
+                document.exitFullscreen();
+            } else if (document.msExitFullscreen)
+            {
+                document.msExitFullscreen();
+            } else if (document.mozCancelFullScreen)
+            {
+                document.mozCancelFullScreen();
+            } else if (document.webkitExitFullscreen)
+            {
+                document.webkitExitFullscreen();
+            }
+        }
+    }
+    document.getElementById("FullScreenButton").addEventListener("click",function () {
+        toggleFullscreen();
+    });
 });
